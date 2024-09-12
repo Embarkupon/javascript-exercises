216138301241bf840bf8d73a8599f6ce4c570e32
Embarkupon <165702416+Embarkupon@users.noreply.github.com>
2024-09-12 15:41:40 -0400

    passed all tests for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if (min < 0 || max < 0 || !Number.isInteger(min) || !Number.isInteger(max)) {
+        return "ERROR";
+    }
+    if (min > max) {
+        return ((min-max+1)/2) * (max+min);
+    }
+    return ((max-min+1)/2) * (max+min);
 };
 
 // Do not edit below this line
