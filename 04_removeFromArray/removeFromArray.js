edd692f8dd39564b228163095c64240456527968
Embarkupon <165702416+Embarkupon@users.noreply.github.com>
2024-09-12 13:44:23 -0400

    passed all tests in 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,25 @@
+// removes an element from an array
 const removeFromArray = function(array, ...args) {
+    // new mutable array
+    let spliced = [];
+    // populates new array with input array's values
+    spliced = array.sort().slice();
+    let curr = null;
+    // iterates through args array
     for (let i = 0; i < args.length; i++) {
-        array.splice(array.indexOf(args[i]), 1);
-        console.log(array);
+        // iterates through input array
+        for (let j = 0; j < array.length; j++) {
+            // if bothe values are strictly equal then remove it from the new array
+            if (args[i] === array[j]) {
+                spliced.splice(spliced.indexOf(args[i]), 1);
+                curr = array[j];
+                console.log (`args (index,element): (${i},${args[i]}). input array (index,element): (${j},${curr})`);
+            }
+        }
+               
     }
-    return array;
+    console.log(`spliced array, [${spliced}]. Input array, [${array}]`);
+    return spliced;
 };
 
 // Do not edit below this line
