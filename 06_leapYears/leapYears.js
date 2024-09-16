1b9f343248e17f7481fa041bb60c1cf77de84181
Embarkupon <165702416+Embarkupon@users.noreply.github.com>
2024-09-16 11:53:45 -0400

    passed all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,16 +1,21 @@
 const leapYears = function(year) {
     if (!Number.isInteger(year)) {
+        console.log(year);
         return false;
     }
     if (year % 4 == 0) {
         if (year % 100 == 0) {
             if (year % 400 == 0) {
+                console.log(`passed third condition: ${year%400}`);
                 return true;
             }
+            console.log(`passed second condition: ${year%100}`);
             return false;
         }
+        console.log(`passed first condition: ${year%4}`);
         return true;
     }
+    return false;
 };
 
 // Do not edit below this line
