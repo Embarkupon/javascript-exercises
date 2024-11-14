44488ba4929ae663a7c38f9e2c437c61fb6fe5b7
Embarkupon <165702416+Embarkupon@users.noreply.github.com>
2024-11-14 17:47:14 -0500

    completed javascript  exercises 08 and 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length == 0) {
+    return 0;
+  }
+	let sum = array.reduce((x, y) => {
+    return x + y;
+  }, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	let product = array.reduce((x, y) => {
+    return x * y;
+  }, 1);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let array = [];
+  if (num == 0) {
+    return 1;
+  }
+  for (let i = 0; i < num; i++) {
+    array.push(i + 1);
+  }
+  let product = array.reduce((x, y) => {
+    return x * y;
+  }, 1);
+  return product;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+    //remove whitespace and special characters
+    let newStr = palindrome.replace(/[^A-Z0-9]/ig,"");
+    newStr = newStr.toLowerCase(); //ignore case
+    //create variables to reverse the string
+    let strArray = newStr.split("");
+    let reverseStr = "";
+    let length = strArray.length;
+    //pop the last character and add it to the reversed string.
+    for (let i = 0; i < length; i++) {
+        reverseStr += strArray.pop().toString();
+    }
+    //if the original and reversed march, the string is a palindrome
+    if (reverseStr.localeCompare(newStr) === 0) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
